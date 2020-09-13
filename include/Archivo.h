6da6ead1def2b9d2296e03eceff687c4cc63c9b5
Jorge Marin <jorgeluis.marin@outlook.com>
2020-09-12 20:54:44 -0500

    Agrega archivo.h y archivo.cpp a project

---
+++ include/Archivo.h
@@ -0,0 +1,16 @@
+#ifndef ARCHIVO_H
+#define ARCHIVO_H
+
+
+class Archivo
+{
+    public:
+        Archivo();
+        virtual ~Archivo();
+
+    protected:
+
+    private:
+};
+
+#endif // ARCHIVO_H
